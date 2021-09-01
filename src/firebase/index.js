a763d522bb12824bf852f09befcad8d3d6da2be0
Tim Jackson <timjacksonm@gmail.com>
2021-08-31 22:09:52 -0500

    installed react router and firebase. Created file structure and removed boilerplate from CRA.

---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// const firebaseApp = initializeApp({
+//     apiKey: '### FIREBASE API KEY ###',
+//     authDomain: '### FIREBASE AUTH DOMAIN ###',
+//     projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+//   });
+
+//   const db = getFirestore();
