1bd291922897b09dce0fa1e6cad223f32a80ff48
Tim Jackson <timjacksonm@gmail.com>
2021-09-01 23:13:37 -0500

    setup firebase firestore config. Updated security rules to only allow reading data. Cant write to db.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './app.css';
+import test from './firebase/index';
 
 function App() {
+  useEffect(() => {
+    test();
+  }, []);
   return (
     <div className="app">
       <div>app</div>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,10 +1,41 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDoc,
+  doc,
+  setDoc,
+} from 'firebase/firestore';
 
-// const firebaseApp = initializeApp({
-//     apiKey: '### FIREBASE API KEY ###',
-//     authDomain: '### FIREBASE AUTH DOMAIN ###',
-//     projectId: '### CLOUD FIRESTORE PROJECT ID ###'
-//   });
+// The Firebase config object contains unique, but non-secret identifiers for your Firebase project.
+// The content of the Firebase config file or object is considered public. Protected by Firebase Security Rules.
+const firebaseConfig = {
+  apiKey: 'AIzaSyDPPrm5Zb9yj6Vm8eGY1jYadRWcMAVa_ds',
+  authDomain: 'where-is-mk.firebaseapp.com',
+  projectId: 'where-is-mk',
+  storageBucket: 'where-is-mk.appspot.com',
+  messagingSenderId: '902783434627',
+  appId: '1:902783434627:web:8c33afcd1922cde6b7c0b6',
+};
 
-//   const db = getFirestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+const test = async () => {
+  const docRef = doc(db, 'characters', 'asdf');
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log('Document data:', docSnap.data());
+  } else {
+    // doc.data() will be undefined in this case
+    console.log('No such document!');
+  }
+};
+
+export default test;
