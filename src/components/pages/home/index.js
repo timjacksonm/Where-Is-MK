2d260ab0fb793480ee371bafe5e0829a78e3f02d
Tim Jackson <timjacksonm@gmail.com>
2021-09-03 21:11:03 -0500

    Added all the single character artwork after editing each image to have a transparent background. Placed in a new data folder/pictures.

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './index.css';
 import Header from './../../header';
 import Footer from './../../footer';
+import { pictures } from '../../../data/pictures/index.js';
 
 const Home = () => {
+  console.log(pictures);
   return (
     <div className="home">
       <Header />
+      {pictures.map((image) => {
+        return <img src={image} alt="" />;
+      })}
       <Footer />
     </div>
   );
